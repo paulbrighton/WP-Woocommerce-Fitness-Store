f832933719055005501eb357bc54cfaa8177dcf0
Paul Brighton <brighton.paul@yahoo.com>
2020-12-04 17:44:13 +0000

    Initial commit

---
+++ lib/navigation.php
@@ -0,0 +1,11 @@
+<?php
+
+function _themename_register_menus() {
+  register_nav_menus( array(
+    'primary' => esc_html__('Primary Menu', '_themename'),
+    'components' => esc_html__('Component Menu', '_themename'),
+    'footer' => __( 'Footer Menu', '_themename' ),
+  ));
+}
+
+add_action( 'init', '_themename_register_menus' );
