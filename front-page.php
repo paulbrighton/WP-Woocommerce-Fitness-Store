af813a1e91c46972779f0ec14aaee54f72bd900d
Paul Brighton <brighton.paul@yahoo.com>
2020-12-08 17:52:16 +0000

    Added contact form css and updated accordion

--- front-page.php
+++ front-page.php
@@ -2,13 +2,14 @@
 <p data-aos="fade-right">Add front page content here</p>
 
 <div class="o-container u-margin-top-50 u-margin-bottom-50">
+  <h1>Accordion</h1>
   <div class="o-row">
     <div class="o-row__col o-row__col--span-12">
       <div class="c-accordion">
         <ul>
           <li>
             <a class="c-accordion--expand">
-              <div class="c-accordion__btn">+</div>
+              <div class="c-accordion__btn"><span><i class="fas fa-angle-double-down fa-xs"></i></span></div>
               <div>
                 <h2 class="c-accordion__header">Header 1</h2>
               </div>
@@ -19,7 +20,7 @@
           </li>
           <li>
             <a class="c-accordion--expand">
-              <div class="c-accordion__btn">+</div>
+              <div class="c-accordion__btn"><span><i class="fas fa-angle-double-down fa-xs"></i></span></div>
               <h2 class="c-accordion__header">Header 2</h2>
             </a>
             <div class="c-accordion__body">
@@ -28,7 +29,7 @@
           </li>
           <li>
             <a class="c-accordion--expand">
-              <div class="c-accordion__btn">+</div>
+              <div class="c-accordion__btn"><span><i class="fas fa-angle-double-down fa-xs"></i></span></div>
               <h2 class="c-accordion__header">Header 3</h2>
             </a>
             <div class="c-accordion__body">
@@ -38,6 +39,35 @@
         </ul>
       </div>
     </div>
+  </div>
+</div>
+
+<!-- Markup for cf7 -->
+<!-- <div class="c-cf7form__inputs">
+  <div class="c-cf7form__input">
+    <label> Name
+      [text* your-name] </label>
+  </div>
+  <div class="c-cf7form__input">
+    <label> Email
+      [email* your-email] </label>
+  </div>
+  <div class="c-cf7form__input">
+    <label> Subject
+      [text* your-subject] </label>
+  </div>
+</div>
+
+<label> Message
+    [textarea your-message 40x5] </label>
+
+[submit "Submit"] -->
+
+<div class="c-contact-form">
+  <div class="o-container">
+    <h1>Contact Form 1</h1>
+    <p class="c-contact-form-subtitle">Three column for name, email and subject.</p>
+    <?php echo do_shortcode('[contact-form-7 id="7" title="Contact form 1"]') ?>
   </div>
 </div>
 <?php get_footer(); ?>
